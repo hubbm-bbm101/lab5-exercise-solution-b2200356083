30a2780b8637e68dc4310c2c7cee9db4fad3f1ec
b2200356083 <92168669+b2200356083@users.noreply.github.com>
2021-11-05 22:36:01 +0300

    Update b2200356083_oddSum_evenAvrg.py
    For better legibility, added an extra blank space to 4 strings that will be written to the screen.

--- b2200356083_oddSum_evenAvrg.py
+++ b2200356083_oddSum_evenAvrg.py
@@ -8,16 +8,16 @@ if maxVal % 2 == 0:
         denum = maxVal/2
     for j in range(1,maxVal,2):
         oddSum += j
-    print("Average of all even numbers in the provided interval is" + str(evenSum/denum))
-    print("Sum of all odd numbers in the provided interval is" + str(oddSum))        
+    print("Average of all even numbers in the provided interval is " + str(evenSum/denum))
+    print("Sum of all odd numbers in the provided interval is " + str(oddSum))        
 else:
     for i in range (0, maxVal,2):
         evenSum += i
         denum = (((maxVal - 1) - 2)/2 + 1)
     for j in range(1,maxVal + 1,2):
         oddSum += j
-    print("Average of all even numbers in the provided interval is" + str(evenSum/denum))
-    print("Sum of all odd numbers in the provided interval is" + str(oddSum))               
+    print("Average of all even numbers in the provided interval is " + str(evenSum/denum))
+    print("Sum of all odd numbers in the provided interval is " + str(oddSum))               
 
 
 
